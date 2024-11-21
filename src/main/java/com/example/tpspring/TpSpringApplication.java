dc65d8d8d4af40e1ade3431e766e6cb29a7b2a51
PLC <pierrelouischaux@gmail.com>
2024-11-21 16:18:49 +0100

    le projet s'exécute, et retourne un Body contenant "Bonjour le monde !" à l'adresse http://localhost:8080/bonjour

--- src/main/java/com/example/tpspring/TpSpringApplication.java
+++ src/main/java/com/example/tpspring/TpSpringApplication.java
@@ -1,13 +1,18 @@
 package com.example.tpspring;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TpSpringApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TpSpringApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TpSpringApplication.class, args);
+    }
+    @GetMapping("/bonjour")
+    public String hello(@RequestParam(value = "name", defaultValue = "le monde ") String name) {
+        return String.format("Bonjour %s!", name);
+    }
 }
